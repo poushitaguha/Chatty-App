a6d385f4ce110b7dac3958b525c38b379a4061c1
poushitaguha <pguha.kolkata@gmail.com>
2019-07-02 19:00:14 +0000

    Initiate data flow in Chatty

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
-import Message from './Message.jsx';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id: "BOB234627663"
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: "ANO999227621"
+        }
+      ]
+    }
+  }
+
+
   render() {
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <main className="messages">
-          <MessageList/>
-          <Message/>
-          <ChatBar/>
-        </main>
+        <MessageList messages = {this.state.messages} />
+        <ChatBar currentUser={this.state.currentUser} />
       </div>    
     );
   }
